58421bc8b2aaf31f66084548400dffde939e4e96
keqi <keqi@standard-robots.com>
2021-11-25 15:35:09 +0800

    新增基础骨架

---
+++ keqi-spring-boot-starter-system/src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+-- ----------------------------
+-- Table structure for sys_config
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_config`;
+CREATE TABLE `sys_config`
+(
+    `config_key`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置项key',
+    `config_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '配置项value',
+    `note`         varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '配置项描述信息',
+    `extra`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '扩展字段',
+    `is_disable`   tinyint(0) UNSIGNED                                           NULL DEFAULT 0 COMMENT '是否禁用[0 未禁用，1 已禁用]',
+    PRIMARY KEY (`config_key`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_dict_item
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dict_item`;
+CREATE TABLE `sys_dict_item`
+(
+    `type_code`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典类型编码',
+    `type_name`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字典类型名称',
+    `item_code`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典项编码',
+    `item_name`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字典项值',
+    `order_num`  int(0)                                                       NULL DEFAULT NULL COMMENT '排序字段',
+    `is_disable` tinyint(0) UNSIGNED                                          NULL DEFAULT 0 COMMENT '是否禁用[0 未禁用，1 已禁用]',
+    PRIMARY KEY (`type_code`, `item_code`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '字典表'
+  ROW_FORMAT = Dynamic;
