8e4d889907a2e91412bd6176266c1d3690c4b25a
keqi <keqi@standard-robots.com>
2021-12-03 16:18:31 +0800

    新增基础骨架

--- keqi-spring-boot-starter-mail/src/main/java/com/keqi/mail/EmailAutoConfiguration.java
+++ keqi-spring-boot-starter-mail/src/main/java/com/keqi/mail/EmailAutoConfiguration.java
@@ -9,11 +9,4 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan("com.keqi.mail")
 public class EmailAutoConfiguration {
-    // 邮件服务信息存储到数据库中，并使用 Spring Cache 做缓存
-
-    // 提供发送普通文本的、静态HTML、thymeleaf模板的方法
-
-    // 动态在内存中创建 JavaMailSenderImpl 对象，并维护起来
-
-    // 并使用 test 方法验证信息是否填写正确
 }
