f078fc7e89b37751330a67344bad0423bf3f804d
keqi <iskeqi@126.com>
2021-12-21 21:46:27 +0800

    新增 spring task starter

--- keqi-spring-boot-starter-task/src/main/java/com/keqi/task/TaskAutoConfiguration.java
+++ keqi-spring-boot-starter-task/src/main/java/com/keqi/task/TaskAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.keqi.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -23,6 +24,7 @@ public class TaskAutoConfiguration {
      *
      * @return r
      */
+    @Bean
     public SchedulingConfigurer schedulingConfiguration() {
         return taskRegistrar -> {
             ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
