98624d5d260ab80276508b579471d22b4628d9bf
keqi <keqi@standard-robots.com>
2021-12-29 14:52:01 +0800

    新增 oss starter

--- keqi-spring-boot-starter-oss/src/main/java/com/keqi/oss/OssProperties.java
+++ keqi-spring-boot-starter-oss/src/main/java/com/keqi/oss/OssProperties.java
@@ -17,11 +17,66 @@ public class OssProperties {
      */
     private String fileStorageType = "localFileSystem";
 
+    /**
+     * Minio 相关属性
+     */
+    private Minio minio = new Minio();
+
+    public static class Minio {
+        private String endpoint;
+
+        private String accessKey;
+
+        private String secretKey;
+
+        private String defaultBucket;
+
+        public String getEndpoint() {
+            return endpoint;
+        }
+
+        public void setEndpoint(String endpoint) {
+            this.endpoint = endpoint;
+        }
+
+        public String getAccessKey() {
+            return accessKey;
+        }
+
+        public void setAccessKey(String accessKey) {
+            this.accessKey = accessKey;
+        }
+
+        public String getSecretKey() {
+            return secretKey;
+        }
+
+        public void setSecretKey(String secretKey) {
+            this.secretKey = secretKey;
+        }
+
+        public String getDefaultBucket() {
+            return defaultBucket;
+        }
+
+        public void setDefaultBucket(String defaultBucket) {
+            this.defaultBucket = defaultBucket;
+        }
+    }
+
     public String getFileStorageType() {
         return fileStorageType;
     }
 
     public void setFileStorageType(String fileStorageType) {
         this.fileStorageType = fileStorageType;
+    }
+
+    public Minio getMinio() {
+        return minio;
+    }
+
+    public void setMinio(Minio minio) {
+        this.minio = minio;
     }
 }
--- keqi-spring-boot-starter-oss/src/main/java/com/keqi/oss/service/oss/MinioService.java
+++ keqi-spring-boot-starter-oss/src/main/java/com/keqi/oss/service/oss/MinioService.java
@@ -1,8 +1,13 @@
 package com.keqi.oss.service.oss;
 
+import com.keqi.oss.OssProperties;
 import com.keqi.oss.domain.dto.DownloadInfoDto;
 import com.keqi.oss.domain.dto.UploadInfoDto;
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * MinioService
@@ -11,6 +16,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MinioService implements OssService {
+
+    @Autowired
+    private OssProperties ossProperties;
+    private MinioClient minioClient;
+
+    @PostConstruct
+    public void init() {
+        minioClient = MinioClient.builder()
+                .endpoint(ossProperties.getMinio().getEndpoint())
+                .credentials(ossProperties.getMinio().getAccessKey(), ossProperties.getMinio().getSecretKey())
+                .build();
+    }
 
     @Override
     public void deleteByName(String fileName) {
@@ -24,6 +41,8 @@ public class MinioService implements OssService {
 
     @Override
     public UploadInfoDto uploadFile(String fileName) {
+        //minioClient.getPresignedPostFormData()
+
         return null;
     }
 }
