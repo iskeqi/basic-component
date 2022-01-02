337507002871a635d91e0e3a47c7b145e4d3d694
keqi <iskeqi@126.com>
2022-01-02 19:48:28 +0800

    新增 spring tdengine starter

--- keqi-spring-boot-starter-tdengine/src/main/java/com/keqi/satoken/mapper/DbMapper.java
+++ keqi-spring-boot-starter-tdengine/src/main/java/com/keqi/satoken/mapper/DbMapper.java
@@ -1,11 +1,23 @@
 package com.keqi.satoken.mapper;
 
+import com.keqi.satoken.domain.param.CreateDbParam;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+import java.util.Map;
+
 public interface DbMapper {
 
-    void createDb();
+    @Update("create database if not exists #{dbName} keep ${keep} days ${days} update #{update}")
+    void createDb(CreateDbParam param);
 
+    @Delete("drop database if exists #{dbName}")
     void deleteDb();
 
+    @Select("show databases;")
+    List<Map<String, Object>> showDatabases();
 
 
 }
