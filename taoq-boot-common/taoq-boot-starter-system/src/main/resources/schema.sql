e6cdaf8b20333d8777c408ceeb4b1b9a8f9b93da
keqi <keqi@standard-robots.com>
2022-03-10 10:14:56 +0800

    修改 sql

--- taoq-boot-common/taoq-boot-starter-system/src/main/resources/schema.sql
+++ taoq-boot-common/taoq-boot-starter-system/src/main/resources/schema.sql
@@ -1,17 +1,19 @@
 drop table if exists sys_config;
-create table sys_config
+create table `sys_config`
 (
-    id           bigint unsigned not null comment '主键id',
-    config_key   varchar(32) comment '配置项key',
-    config_value varchar(32) comment '配置项value',
-    note         varchar(128) comment '配置项描述信息',
-    extra        varchar(128) comment '扩展字段',
-    is_disable   tinyint unsigned default 0 comment '是否禁用[0:未禁用 1:已禁用]',
-    create_time  datetime comment '创建时间',
-    update_time  datetime comment '修改时间',
-    primary key (id)
-) comment = '系统配置表';
-create unique index uk_config_key on sys_config(config_key);
+    `id`           bigint unsigned not null comment '主键id',
+    `config_key`   varchar(32)      default null comment '配置项key',
+    `config_value` varchar(32)      default null comment '配置项value',
+    `note`         varchar(128)     default null comment '配置项描述信息',
+    `extra`        varchar(128)     default null comment '扩展字段',
+    `is_disable`   tinyint unsigned default '0' comment '是否禁用[0:未禁用 1:已禁用]',
+    `create_time`  datetime         default null comment '创建时间',
+    `update_time`  datetime         default null comment '修改时间',
+    primary key (`id`),
+    unique key `uk_config_key` (`config_key`)
+) engine = innodb
+  default charset = utf8mb4
+  collate = utf8mb4_0900_ai_ci comment ='系统配置表';
 
 drop table if exists sys_dict_item;
 create table sys_dict_item
