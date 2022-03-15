61648c25f288cdb2b1530f7d22e4a328238e9d1e
keqi <keqi@standard-robots.com>
2022-03-15 15:45:55 +0800

    修改 sql

--- taoq-boot-common/taoq-boot-starter-common/src/main/java/tech/taoq/common/response/ResultStatusEnum.java
+++ taoq-boot-common/taoq-boot-starter-common/src/main/java/tech/taoq/common/response/ResultStatusEnum.java
@@ -10,15 +10,20 @@ public enum ResultStatusEnum {
     SUCCESS("00000", "success"),
 
     // 用户端错误
-    CLINET_ERROR("A0001", "用户端错误"),
-    NO_AUTH("A0002", "token错误或失效"),
-    PARAM_ILLEGAL("A0003", "参数错误"),
+
+    // 客户端错误
+    CLINET_ERROR("A0001", "client error"),
+    // token 失效或错误
+    NO_AUTH("A0002", "token invalid or error"),
+    // 参数错误
+    PARAM_ILLEGAL("A0003", "param error"),
 
     // 服务端错误
-    SERVER_ERROR("B0001", "系统繁忙，请稍后重试"),
+    // 系统繁忙，请稍后重试
+    SERVER_ERROR("B0001", "system is busy, please try again later"),
 
     // 第三方服务错误
-    THIRD_SERVICE_ERROR("C0001", "调用第三方服务出错");
+    THIRD_SERVICE_ERROR("C0001", "error in third party service");
 
     /**
      * 状态码
