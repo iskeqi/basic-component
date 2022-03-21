1ac57d8a77bc88d6bcb525d56a7979580974949e
keqi <keqi@standard-robots.com>
2022-03-21 15:04:06 +0800

    修改字典表 sql

--- taoq-boot-common/taoq-boot-starter-system/src/main/resources/schema.sql
+++ taoq-boot-common/taoq-boot-starter-system/src/main/resources/schema.sql
@@ -1,32 +1,44 @@
-drop table if exists sys_config;
-create table `sys_config`
+create database if not exists `taoq-boot` default charset utf8mb4 collate utf8mb4_0900_ai_ci;
+
+create table if not exists `sys_config`
 (
     `id`           bigint unsigned not null comment '主键id',
-    `config_key`   varchar(32)      default null comment '配置项key',
-    `config_value` varchar(32)      default null comment '配置项value',
-    `note`         varchar(128)     default null comment '配置项描述信息',
-    `extra`        varchar(128)     default null comment '扩展字段',
+    `config_key`   varchar(32)  default null comment '配置项key',
+    `config_value` varchar(32)  default null comment '配置项value',
+    `note`         varchar(128) default null comment '配置项描述信息',
+    `extra`        varchar(128) default null comment '扩展字段',
     `is_disable`   tinyint unsigned default '0' comment '是否禁用[0:未禁用 1:已禁用]',
-    `create_time`  datetime         default null comment '创建时间',
-    `update_time`  datetime         default null comment '修改时间',
+    `create_time`  datetime     default null comment '创建时间',
+    `update_time`  datetime     default null comment '修改时间',
     primary key (`id`),
     unique key `uk_config_key` (`config_key`)
-) engine = innodb
-  default charset = utf8mb4
-  collate = utf8mb4_0900_ai_ci comment ='系统配置表';
+) engine=innodb default charset=utf8mb4 collate=utf8mb4_0900_ai_ci comment='配置表';
 
-drop table if exists sys_dict_item;
-create table sys_dict_item
+create table if not exists `sys_dict_type`
 (
-    id          bigint unsigned not null comment '主键id',
-    type_code   varchar(32) comment '字典类型编码',
-    type_name   varchar(32) comment '字典类型名称',
-    item_code   varchar(32) comment '字典项编码',
-    item_name   varchar(32) comment '字典项值',
-    order_num   int comment '排序字段',
-    is_disable  tinyint unsigned default 0 comment '是否禁用[0:未禁用 1:已禁用]',
-    create_time datetime comment '创建时间',
-    update_time datetime comment '修改时间',
-    primary key (id)
-) comment = '字典表';
-create unique index uk_type_item on sys_dict_item (type_code, item_code);
+    `id`          bigint unsigned not null comment '主键id',
+    `name`        varchar(32)  default '' comment '字典名称',
+    `type`        varchar(32)  default '' comment '字典类型',
+    `note`        varchar(500) default null comment '备注',
+    `is_disable`  tinyint unsigned default '0' comment '是否禁用[0:未禁用 1:已禁用]',
+    `create_time` datetime     default null comment '创建时间',
+    `update_time` datetime     default null comment '更新时间',
+    primary key (`id`) using btree,
+    unique key `dict_type` (`type`)
+) engine=innodb default charset=utf8mb4 collate=utf8mb4_0900_ai_ci comment='字典类型表';
+
+create table `sys_dict_item`
+(
+    `id`          bigint unsigned not null comment '主键id',
+    `item_code`   varchar(32)  default null comment '字典项编码',
+    `item_name`   varchar(32)  default '' comment '字典项名称',
+    `item_value`  varchar(32)  default '' comment '字典项值',
+    `dict_type`   varchar(32)  default '' comment '字典类型',
+    `note`        varchar(500) default null comment '备注',
+    `order_num`   int          default '0' comment '字典排序',
+    `is_disable`  tinyint unsigned default '0' comment '是否禁用[0:未禁用 1:已禁用]',
+    `create_time` datetime     default null comment '创建时间',
+    `update_time` datetime     default null comment '更新时间',
+    primary key (`id`) using btree,
+    unique key `uk_dict_type_item_code` (`item_code`,`dict_type`)
+) engine=innodb default charset=utf8mb4 collate=utf8mb4_0900_ai_ci comment='字典数据表';
