40849d168195092452e3f8cfb096f0a02df1d637
keqi <iskeqi@126.com>
2022-04-17 21:37:13 +0800

    新增 taoq-boot-starter-sso 实现

---
+++ taoq-boot-common/taoq-boot-starter-sso/src/main/java/tech/taoq/sso/SsoAutoConfiguration.java
@@ -0,0 +1,12 @@
+package tech.taoq.sso;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author keqi
+ */
+@ComponentScan("tech.taoq.sso")
+public class SsoAutoConfiguration {
+
+
+}
