f220c3a07161e7a7fea82c2fc5ca40539c38fef0
keqi <iskeqi@126.com>
2022-05-31 17:57:03 +0800

    新增 taoq-boot-starter-rbac 实现

--- taoq-boot-common/taoq-boot-starter-system/src/main/java/tech/taoq/system/SystemAutoConfiguration.java
+++ taoq-boot-common/taoq-boot-starter-system/src/main/java/tech/taoq/system/SystemAutoConfiguration.java
@@ -3,6 +3,8 @@ package tech.taoq.system;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
+ * SystemAutoConfiguration
+ *
  * @author keqi
  */
 @ComponentScan("tech.taoq.system")
