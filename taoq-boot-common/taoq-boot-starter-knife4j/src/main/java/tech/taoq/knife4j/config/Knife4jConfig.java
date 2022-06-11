2df08fd9d0fcb922e71ab3acc07373587d2d9a24
keqi <iskeqi@126.com>
2022-06-12 01:12:05 +0800

    新增 taoq-boot-starter-rbac 实现

--- taoq-boot-common/taoq-boot-starter-knife4j/src/main/java/tech/taoq/knife4j/config/Knife4jConfig.java
+++ taoq-boot-common/taoq-boot-starter-knife4j/src/main/java/tech/taoq/knife4j/config/Knife4jConfig.java
@@ -1,5 +1,7 @@
 package tech.taoq.knife4j.config;
 
+import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,17 @@ import springfox.documentation.spring.web.plugins.Docket;
  */
 public class Knife4jConfig {
 
+    private final OpenApiExtensionResolver openApiExtensionResolver;
+
+    @Autowired
+    public Knife4jConfig(OpenApiExtensionResolver openApiExtensionResolver) {
+        this.openApiExtensionResolver = openApiExtensionResolver;
+    }
+
     @Bean
     @ConditionalOnMissingBean
     public Docket defaultApi2() {
+        String groupName = "default group";
         return new Docket(DocumentationType.SWAGGER_2)
                 // 关闭默认的响应信息
                 .useDefaultResponseMessages(false)
@@ -27,11 +37,12 @@ public class Knife4jConfig {
                         .termsOfServiceUrl("https://doc.xiaominfo.com/")
                         .version("1.0")
                         .build())
-                .groupName("default group")
+                .groupName(groupName)
                 .select()
                 // 所有使用了 @RestController 注解的类都会被扫描出来
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .extensions(openApiExtensionResolver.buildExtensions(groupName));
     }
 }
