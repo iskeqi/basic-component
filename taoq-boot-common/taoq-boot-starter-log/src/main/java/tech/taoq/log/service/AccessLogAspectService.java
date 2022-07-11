d8e5e784415ce0a4e084a2cf9457f6f0f45425cc
keqi <iskeqi@126.com>
2022-07-11 15:07:18 +0800

    修改日志记录方式

--- taoq-boot-common/taoq-boot-starter-log/src/main/java/tech/taoq/log/service/AccessLogAspectService.java
+++ taoq-boot-common/taoq-boot-starter-log/src/main/java/tech/taoq/log/service/AccessLogAspectService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 import tech.taoq.log.domain.bo.AccessLog;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * AccessLogAspectService
@@ -18,15 +16,13 @@ import java.util.concurrent.Executors;
 @ConditionalOnProperty(prefix = "taoq.log.access-log", value = "enable")
 public class AccessLogAspectService {
 
-    private static final ExecutorService LOG_EXECUTOR_SERVICE = Executors.newFixedThreadPool(2);
-
     @Autowired(required = false)
     private List<AccessLogService> accessLogServiceList;
 
     public void writeLog(AccessLog accessLog) {
         if (accessLogServiceList != null && accessLogServiceList.size() > 0) {
             for (AccessLogService accessLogService : accessLogServiceList) {
-                LOG_EXECUTOR_SERVICE.submit(() -> accessLogService.writeAccessLog(accessLog));
+                accessLogService.writeAccessLog(accessLog);
             }
         }
     }
