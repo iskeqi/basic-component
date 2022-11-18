5c8c8e2d9e1ca53216abdf40c61bb638ae6d0971
keqi <iskeqi@126.com>
2022-11-18 15:09:24 +0800

    1、新增 flyway/p6spy 依赖

--- taoq-boot-test/tbt-bootstrap/src/main/java/com/keqi/tbt/Application.java
+++ taoq-boot-test/tbt-bootstrap/src/main/java/com/keqi/tbt/Application.java
@@ -3,12 +3,14 @@ package com.keqi.tbt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator;
 
 @MapperScan("com.keqi.tbt.**.mapper")
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        CachingOperationNameGenerator cachingOperationNameGenerator;
         SpringApplication.run(Application.class, args);
     }
 }
