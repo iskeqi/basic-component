1037085a7bd665874ab32c1ac3e1f58ef12b6d92
keqi <iskeqi@126.com>
2022-11-21 14:06:25 +0800

    1、新增 spring-boot-starter-test

---
+++ taoq-boot-test/tbt-bootstrap/src/test/java/com/keqi/tbt/ConfigTest.java
@@ -0,0 +1,13 @@
+package com.keqi.tbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigTest {
+
+    @Test
+    public void test() {
+
+    }
+}
