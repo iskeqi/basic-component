9b45b70694d38da762d42f084093feaf411f2ce3
keqi <iskeqi@126.com>
2022-11-21 11:10:15 +0800

    1、字符串编解码方法,新增 反转 参数

--- taoq-boot-common/taoq-boot-starter-modbus/src/main/java/tech/taoq/modbus/AbstractModbusMasterTCP.java
+++ taoq-boot-common/taoq-boot-starter-modbus/src/main/java/tech/taoq/modbus/AbstractModbusMasterTCP.java
@@ -280,10 +280,11 @@ public abstract class AbstractModbusMasterTCP {
     /**
      * 计算 16bit 的整数对应的 ASCII 编码的字符串表示
      *
-     * @param param 十进制的 16bit 整数
+     * @param param   十进制的 16bit 整数
+     * @param reverse 是否需要反转[true:低8位在前,高8位在后 false:低8位在后,高8位在前]
      * @return 十进制对应的 ASCII 编码的字符串
      */
-    final public String shortToString(short param) {
+    final public String shortToString(short param, boolean reverse) {
 
         // [高八位数据]右移8位后,取 int 类型的最低8位,其它抛弃
         byte a = (byte) (param >> 8);
@@ -292,17 +293,18 @@ public abstract class AbstractModbusMasterTCP {
         byte b = (byte) param;
 
         // 拼接成字符串时,低8位的数据放到前面,高8位的数据放到后面
-        byte[] bytes = {b, a};
+        byte[] bytes = reverse ? new byte[]{b, a} : new byte[]{a, b};
         return new String(bytes);
     }
 
     /**
      * 计算 ASCII 编码字符串对应的二进制数据的十进制表示
      *
-     * @param param ASCII 编码字符串
+     * @param param   ASCII 编码字符串
+     * @param reverse 是否需要反转[true:低8位在前,高8位在后 false:低8位在后,高8位在前]
      * @return ASCII 编码的字符串对应的十进制整数
      */
-    final public short stringToShort(String param) {
+    final public short stringToShort(String param, boolean reverse) {
         if (param.length() != 2) {
             throw new RuntimeException("本方法仅支持长度位2的字符串");
         }
@@ -315,17 +317,17 @@ public abstract class AbstractModbusMasterTCP {
         String str2 = String.format("%08d", Integer.parseInt(Integer.toString(bytes[1], 2)));
 
         // 16 个 bit 的二进制字符串转成对应的 int 类型整数,低8位放在前面,高8位放到后面
-
-        return Short.parseShort(str2 + str1, 2);
+        return Short.parseShort(reverse ? str2 + str1 : str1 + str2, 2);
     }
 
     /**
      * 计算 32bit 的整数对应的 ASCII 编码的字符串表示
      *
-     * @param param 十进制的 16bit 整数
+     * @param param   十进制的 16bit 整数
+     * @param reverse 是否需要反转[true:低8位在前,高8位在后 false:低8位在后,高8位在前]
      * @return 十进制对应的 ASCII 编码的字符串
      */
-    final public String intToString(int param) {
+    final public String intToString(int param, boolean reverse) {
 
         // [从右开始,第1个八位数据]取 int 类型的最低8位,其它抛弃
         byte d = (byte) param;
@@ -340,7 +342,7 @@ public abstract class AbstractModbusMasterTCP {
         byte a = (byte) (param >> 24);
 
         // 拼接成字符串时,低8位的数据放到前面,高8位的数据放到后面
-        byte[] bytes = {d, c, b, a};
+        byte[] bytes = reverse ? new byte[]{d, c, b, a} : new byte[]{a, b, c, d};
 
         return new String(bytes);
     }
@@ -348,10 +350,11 @@ public abstract class AbstractModbusMasterTCP {
     /**
      * 计算 ASCII 编码字符串对应的二进制数据的十进制表示
      *
-     * @param param ASCII 编码字符串
+     * @param param   ASCII 编码字符串
+     * @param reverse 是否需要反转[true:低8位在前,高8位在后 false:低8位在后,高8位在前]
      * @return ASCII 编码的字符串对应的十进制整数
      */
-    final public int stringToInt(String param) {
+    final public int stringToInt(String param, boolean reverse) {
         if (param.length() != 4) {
             throw new RuntimeException("本方法仅支持长度位4的字符串");
         }
@@ -366,8 +369,7 @@ public abstract class AbstractModbusMasterTCP {
         String str4 = String.format("%08d", Integer.parseInt(Integer.toString(bytes[3], 2)));
 
         // 16 个 bit 的二进制字符串转成对应的 int 类型整数,低8位放在前面,高8位放到后面
-
-        return Integer.parseInt(str4 + str3 + str2 + str1, 2);
+        return Integer.parseInt(reverse ? str4 + str3 + str2 + str1 : str1 + str2 + str3 + str4, 2);
     }
 
 
