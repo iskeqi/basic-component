cc02196c4567ef1415fea7dae3cb5f85163e8419
keqi <iskeqi@126.com>
2022-11-26 11:16:00 +0800

    1、新增 validator 全局异常捕获

--- taoq-boot-common/taoq-boot-starter-web/src/main/java/tech/taoq/web/mvc/exception/GlobalExceptionHandler.java
+++ taoq-boot-common/taoq-boot-starter-web/src/main/java/tech/taoq/web/mvc/exception/GlobalExceptionHandler.java
@@ -2,13 +2,20 @@ package tech.taoq.web.mvc.exception;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.taoq.common.exception.TaoqException;
 import tech.taoq.common.response.ResultEntity;
 import tech.taoq.common.response.ResultEntityBuilder;
+import tech.taoq.common.response.ResultStatusEnum;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * 全局异常处理器
@@ -22,7 +29,7 @@ public class GlobalExceptionHandler {
 
     @Autowired
     private List<ExceptionHandlerAdapter> exceptionHandlerAdapterList;
-
+    
     /**
      * TaoqException
      *
@@ -33,6 +40,62 @@ public class GlobalExceptionHandler {
     public ResultEntity<?> businessException(TaoqException e) {
         log.error("status={},message={}", e.getStatus(), e.getMessage());
         return ResultEntityBuilder.failure(e.getStatus(), e.getMessage());
+    }
+
+    /**
+     * 使用 @Validated 校验方法参数中的 @RequestBody 修饰的实体类，抛出的是这种异常
+     * 使用 @Valid 校验方法参数中的 @RequestBody 修饰的实体类，抛出的是这种异常(嵌套多层次校验)
+     * <p>
+     * 总结：使用了 @RequestBody 修饰参数，抛出的就是此种异常
+     *
+     * @param e MethodArgumentNotValidException
+     * @return r
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResultEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringJoiner errorMsg = new StringJoiner(";");
+        for (ObjectError allError : e.getBindingResult().getAllErrors()) {
+            errorMsg.add(allError.getDefaultMessage());
+        }
+
+        return ResultEntityBuilder.failure(ResultStatusEnum.PARAM_ILLEGAL.getCode(), errorMsg.toString());
+    }
+
+    /**
+     * 使用 @Validated 校验方法参数中的实体类参数，抛出的是此种异常(无论方法是 GET 还是 POST)
+     * <p>
+     * (GET请求有多个参数时，可以使用此种方式接收参数，如果是 POST 建议还是使用 @RequestBody 的方式来接收参数)
+     *
+     * @param e BindException
+     * @return r
+     */
+    @ExceptionHandler(value = BindException.class)
+    public ResultEntity<?> bindException(BindException e) {
+        StringJoiner errorMsg = new StringJoiner(";");
+        for (ObjectError allError : e.getBindingResult().getAllErrors()) {
+            errorMsg.add(allError.getDefaultMessage());
+        }
+
+        return ResultEntityBuilder.failure(ResultStatusEnum.PARAM_ILLEGAL.getCode(), errorMsg.toString());
+    }
+
+    /**
+     * 使用 @Validated 校验方法参数中的 @RequestParam 和 @PathVariable 修饰的参数，抛出的是这种异常(无论方法是 GET 还是 POST)
+     * Controller 方法中没有显式使用 @RequestParam 修饰的非实体类参数，同样抛出的是此种异常
+     * <p>
+     * 总结：使用了 @RequestParam/@PathVariable 修饰参数，抛出的就是此种异常
+     *
+     * @param e ConstraintViolationException
+     * @return r
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResultEntity<?> constraintViolationException(ConstraintViolationException e) {
+        StringJoiner errorMsg = new StringJoiner(";");
+        for (ConstraintViolation<?> constraintViolation : e.getConstraintViolations()) {
+            errorMsg.add(constraintViolation.getMessage());
+        }
+
+        return ResultEntityBuilder.failure(ResultStatusEnum.PARAM_ILLEGAL.getCode(), errorMsg.toString());
     }
 
     /**
